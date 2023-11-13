7d92ba26ca365baf374524ecdd4f06564ce32db6
hxia5 <hxia5@illinois.edu>
2023-11-13 14:22:26 -0500

    a

--- main.py
+++ main.py
@@ -1,29 +1,13 @@
-from mylib.lib import (
-    extract,
-    load,
-    describe,
-    group_win_prob_query,
-    transform,
-)
-from pyspark.sql import SparkSession
-
-
-def main():
-    # extract data
-    extract()
-    # start spark session
-    spark = SparkSession.builder.appName("WorldCupPredictions").getOrCreate()
-    # load data into dataframe
-    df = load(spark)
-    # example metrics
-    describe(df)
-    # query
-    group_win_prob_query(df,"world_cup_data")
-    # example transform
-    transform(df)
-    # end spark session
-    spark.stop()
+from mylib.extract import extract
+from mylib.load import load
+from mylib.query_visual import query_transform, vis
+import os 
 
 
 if __name__ == "__main__":
-    main()
+    current_directory = os.getcwd()
+    print(current_directory)
+    extract()
+    load()
+    query_transform()
+    vis()
--- test_main.py
+++ test_main.py
@@ -2,56 +2,34 @@
 Test goes here
 
 """
+"""
+Test databricks fucntionaility
+"""
+import requests
+from dotenv import load_dotenv
 import os
-import pytest
-from mylib.lib import (
-    extract,
-    load,
-    describe,
-    group_win_prob_query,
-    transform,
-)
-from pyspark.sql import SparkSession
 
-@pytest.fixture(scope="module")
-def spark():
-    spark = SparkSession.builder.appName("WorldCupPredictions").getOrCreate()
-    yield spark
-    spark.stop()
+# Load environment variables
+load_dotenv()
+server_h = os.getenv("SERVER_HOSTNAME")
+access_token = os.getenv("TOKEN")
+FILESTORE_PATH = "dbfs:/FileStore/individual_project3"
+url = f"https://{server_h}/api/2.0"
 
-def test_extract():
-    file_path = extract()
-    assert os.path.exists(file_path) is True
+# Function to check if a file path exists and auth settings still work
+def check_filestore_path(path, headers): 
+    try:
+        response = requests.get(url + f"/dbfs/get-status?path={path}", headers=headers)
+        response.raise_for_status()
+        return response.json()['path'] is not None
+    except Exception as e:
+        print(f"Error checking file path: {e}")
+        return False
 
-
-def test_load(spark):
-    df = load(spark)
-    assert df is not None
-
-
-def test_describe(spark):
-    df = load(spark)
-    result = describe(df)
-    assert result is None
-
-
-def test_query(spark):
-    df = load(spark)
-    name = "world_cup_data"
-    result = group_win_prob_query(df, name)
-    assert result is None
-
-
-def test_transform(spark):
-    df = load(spark)
-    result = transform(df)
-    assert result is None
-
+# Test if the specified FILESTORE_PATH exists
+def test_databricks():
+    headers = {'Authorization': f'Bearer {access_token}'}
+    assert check_filestore_path(FILESTORE_PATH, headers) is True
 
 if __name__ == "__main__":
-    #pytest.main(["-v", "test_main.py"])
-    test_extract()
-    test_load(spark)
-    test_describe(spark)
-    test_query(spark)
-    test_transform(spark)
+    test_databricks()
